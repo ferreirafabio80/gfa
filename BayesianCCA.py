7c9c769e063e3cb6ff453b8e8d5d0b4f22f0cf28
Fabio FERREIRA <fferreir@coast.cs.ucl.ac.uk>
2019-01-22 19:01:03 +0000

    Initialising parameters

--- BayesianCCA.py
+++ BayesianCCA.py
@@ -4,10 +4,48 @@ from scipy.stats import norm, gamma, wishart
 
 class BayesianCCA(object):
 
-    def __init__(self, X1, X2, a_alpha=10e-3, b_alpha=10e-3, 
-                 a_tau=10e-3, b_tau=10e-3, beta=10e-3):
+    def __init__(self, d, N, a, b, beta, K, nu):
 
-        d1 = X1.shape[1]  # number of dimensions of X1
-        d2 = X2.shape[1]  # number of dimensions of X2
-        m = np.min(d1,d2) # number of different models 
-        N = X1.shape[0]   # number of data points
+        self.d = np.zeros(1,d.shape[1])
+        for i in range(0,d.shape[1]):
+            self.d[i] = d[i]  # number of dimensions of data sources
+        m = np.min(d)    
+        self.m = m   # number of different models 
+        self.N = N   # number of data points
+
+        # Hyperparameters
+        self.a = a
+        self.b = b
+        self.beta = beta
+        self.K = K
+        self.nu = nu
+        
+        # Variational parameters
+        self.means_z = np.random.randn(m, N)
+        self.sigma_z = np.random.randn(m, m)
+        self.mean_mu = [[] for _ in range(d.shape[1])]
+        self.sigma_mu = [[] for _ in range(d.shape[1])]
+        self.means_w = [[] for _ in range(d.shape[1])]
+        self.sigma_w = [[] for _ in range(d.shape[1])]
+        self.a_new = [[] for _ in range(d.shape[1])]
+        self.b_new = [[] for _ in range(d.shape[1])]
+        self.K_new = [[] for _ in range(d.shape[1])]
+        self.nu_new = [[] for _ in range(d.shape[1])]
+        for i in range(0,d.shape[1]):
+            self.mean_mu[i] = np.random.randn(d[i], 1)
+            self.sigma_mu[i] = np.random.randn(d[i], d[i])
+            self.means_w[i] = np.random.randn(d[i], m)
+            self.sigma_w[i] = np.random.randn(m, m)
+            self.a_new[i] = self.a + d[i]/2.0
+            self.b_new[i] = np.abs(np.random.randn(m, 1))
+            self.K_new[i] = np.abs(np.random.randn(d[i], d[i]))
+            self.nu_new[i] = self.nu + N
+
+    def update_z(self, X):
+        E_tau = self.a_tau_tilde / self.b_tau_tilde
+        E_W = self.means_w
+        E_mu = self.mean_mu
+        for n in range(0,self.N):
+            t_n = np.reshape(X.T[n],(self.d,1)) 
+            self.means_z.T[n] = reshape(E_tau * dot ( dot( self.sigma_z , E_W.T ) , (t_n - E_mu)) ,self.q)
+        self.sigma_z = np.linalg.inv(np.identity(self.q) + E_tau*(dot(E_W.T,E_W)))
--- BayesianPCA.py
+++ BayesianPCA.py
@@ -1,7 +1,5 @@
-import numpy as np
 import scipy.special as sp
 import scipy.special as special
-
 class BayesianPCA(object):
     
     def __init__(self, d, N, a_alpha=10e-3, b_alpha=10e-3, a_tau=10e-3, b_tau=10e-3, beta=10e-3):
@@ -26,20 +24,15 @@ class BayesianPCA(object):
         self.sigma_mu = np.random.randn(d, d)
         self.means_w = np.random.randn(d, q)
         self.sigma_w = np.random.randn(q, q)
-        self.a_alpha_tilde = np.abs(np.random.randn(1))
+        self.a_alpha_tilde = self.a_alpha + d/2.0
         self.bs_alpha_tilde = np.abs(np.random.randn(q, 1))
-        self.a_tau_tilde = np.abs(np.random.randn(1))
+        self.a_tau_tilde = self.a_tau + (N*d)/2.0
         self.b_tau_tilde = np.abs(np.random.randn(1))
     
     def __update_z(self, X):
-        E_tau = self.a_tau_tilde / self.b_tau_tilde
-        E_W = self.means_w
-        E_mu = self.mean_mu
-        for n in range(0,self.N):
-            t_n = np.reshape(X.T[n],(self.d,1)) 
-            self.means_z.T[n] = reshape(E_tau * dot ( dot( self.sigma_z , E_W.T ) , (t_n - E_mu)) ,self.q)
-        self.sigma_z = np.linalg.inv(np.identity(self.q) + E_tau*(dot(E_W.T,E_W)))
-        
+        self.sigma_z = np.linalg.inv(np.identity(self.d) + (self.a_tau_tilde/self.b_tau_tilde)*( np.trace(self.sigma_w) + dot(self.means_w.T,self.means_w)))
+        self.means_z = (self.a_tau_tilde/self.b_tau_tilde)*dot(dot(self.sigma_z,self.means_w.T) , (X - self.mean_mu))
+    
     def __update_mu(self,X):
         E_tau = self.a_tau_tilde / self.b_tau_tilde
         E_W = self.means_w
@@ -49,44 +42,34 @@ class BayesianPCA(object):
             x_n = np.reshape(self.means_z.T[n],(self.q,1)) 
             S += t_n - dot(E_W,x_n)
         self.mean_mu = E_tau*dot(self.sigma_mu,S)
-        self.sigma_mu = np.identity(self.d) / (self.beta + self.N*(self.a_tau_tilde/self.b_tau_tilde))
-    
+        self.sigma_mu = np.identity(self.d) / (self.beta + self.N*(self.a_tau_tilde/self.b_tau_tilde)) 
+        
     def __update_w(self, X):
-        
-        S = 0
-        for n in range(0,self.N):
-            x_n = np.reshape(self.means_z.T[n],(self.q,1)) 
-            S += self.sigma_z + dot(x_n,x_n.T)
-        self.sigma_w = np.linalg.inv( diagflat(self.a_alpha_tilde/self.bs_alpha_tilde) + (self.a_tau_tilde/self.b_tau_tilde)*(S))
-        
-        
+        self.sigma_w = np.linalg.inv( diagflat(self.a_alpha_tilde/self.bs_alpha_tilde) + (self.a_tau_tilde/self.b_tau_tilde)*(self.N*self.sigma_z + dot(self.means_z,self.means_z.T) ) )
         E_tau = self.a_tau_tilde / self.b_tau_tilde
-        S = 0
         for k in range(0,self.d):
+            S = 0
             mu_k = self.mean_mu[k]
             for n in range(0,self.N):
                 t_n_k = X.T[n][k]
                 x_n = np.reshape(self.means_z.T[n],(self.q,1))
                 S += x_n*(t_n_k - mu_k)
             self.means_w[k] = np.reshape( E_tau* dot(self.sigma_w,S) , self.q )
-                    
-        
+    
     
     def __update_alpha(self):
-        self.a_alpha_tilde = self.a_alpha + self.d/2
         for i in range(0 , self.q):
-            self.bs_alpha_tilde[i] = self.b_alpha + (np.trace(np.cov(self.means_w)) +dot(self.means_w.T[i].T,self.means_w.T[i])) / 2
+            self.bs_alpha_tilde[i] = self.b_alpha + (np.trace(self.sigma_w)+dot(self.means_w.T[i].T,self.means_w.T[i])) / 2
          
         
     def __update_tau(self, X):
-        self.a_tau_tilde = self.a_tau + self.N*self.d / 2
         A = 0
         E_W = self.means_w
         for i in range(0,self.N):
             t_n = np.reshape(X.T[i],(self.d,1))
             x_n = np.reshape(self.means_z.T[i],(self.q,1))
             A += dot(t_n.T,t_n) + np.trace(self.sigma_mu) + dot(self.mean_mu.T,self.mean_mu)
-            A += np.trace( dot(dot(E_W.T,E_W), self.sigma_z + dot(x_n,x_n.T)) )
+            A += np.trace( dot( np.trace(self.sigma_w) + dot(E_W.T,E_W), self.sigma_z + dot(x_n,x_n.T)) )
             A += 2*dot(dot(self.mean_mu.T,self.means_w),x_n)
             A += -2* dot(dot(t_n.T,self.means_w),x_n) - 2*dot(t_n.T,self.mean_mu)
         self.b_tau_tilde = self.b_tau + 0.5*A
@@ -96,89 +79,64 @@ class BayesianPCA(object):
         L = 0
         ###Terms from expectations###
         #N(X_n|Z_n)
+        L += -self.N*self.d/2 * (special.digamma(self.a_tau_tilde) - np.log(self.b_tau_tilde)) - self.d/2 *np.log(2*np.pi)
+        S = 0
         for n in range(0,self.N):
-            L += dot(X[:,n].T, X[:,n])
-            L += dot(X[:,n].T, self.mean_mu)[0]
-            #L += dot(dot(self.means_w.T[n].T, self.means_w[:,n]), dot(self.means_z[:,n].T, self.means_z[:,n]))
-            L += dot(self.mean_mu.T, X[:,n].T)[0]
-            L -= dot (dot (self.mean_mu.T, self.means_w), self.means_z[:,n])[0]
-            L += np.trace(self.sigma_mu) + dot(self.mean_mu.T, self.mean_mu)[0][0]
-        #print L
-        
-        #TODO: add this
+            S += dot(X[:,n].T, X[:,n])
+            S += dot(dot(X[:,n].T,self.means_w),self.means_z[:,n]) 
+            S += dot(X[:,n].T, self.mean_mu)[0]
+            S -= dot(dot(self.means_w.T,self.means_z[:,n].T) ,X[:,n])
+            S -= dot(dot(self.means_w.T,self.means_z[:,n].T) ,self.mean_mu)    
+            S += dot( self.mean_mu.T,X[:,n])[0]
+            S -= dot(dot(self.mean_mu.T,self.means_w) ,self.means_z[:,n]) 
+            S += np.trace(self.sigma_mu) + dot( self.mean_mu.T,self.mean_mu)[0]
+            S += dot(self.mean_mu.T, X[:,n].T)[0]
+            S -= dot (dot (self.mean_mu.T, self.means_w), self.means_z[:,n])[0]
+            S += np.trace(self.sigma_mu) + dot(self.mean_mu.T, self.mean_mu)[0][0]
+        L +=  self.N/2 *(self.a_tau_tilde/self.b_tau_tilde)*S
         
         #sum ln N(z_n)
-        L += - self.N / 2 * np.trace(self.sigma_z)
+        L += - self.N / 2 * np.trace(self.sigma_z) - self.N*self.d/2 *np.log(2*np.pi)
         for n in range(0,self.N):
             L += - 1/2 * dot(self.means_z.T[n].T, self.means_z.T[n])
         
-#         print "first"
-        #print 1, L
-            
-        #sum ln(N)
+        #sum ln(W|a)
+        L += self.q * self.d /2 * (- np.log(2*np.pi) )
+        for i in range(0, self.q):
+            L+= -self.q /2 * ((special.digamma(self.a_alpha_tilde) - log(self.bs_alpha_tilde[i][0]) ) + self.a_alpha_tilde / self.bs_alpha_tilde[i][0]) /   (- 1/2) *  (np.trace(self.sigma_w) + dot(self.means_w.T[i].T, self.means_w.T[i]))
         
-        for i in range(0, self.d):
-            L+= -self.d /2 * (special.digamma(self.a_alpha_tilde) - log(self.bs_alpha_tilde[i][0]) ) - 1/2 * (np.trace(self.sigma_w) + dot(self.means_w.T[i].T, self.means_w.T[i]))
-
-
-       # print 2, L
         #sum ln (Ga(a_i))
-        for i in range(0, self.d):
-            L += (self.a_alpha - 1) * ( -log(self.bs_alpha_tilde[i][0]) + special.digamma(self.a_alpha_tilde))- self.b_alpha * (self.a_alpha_tilde / self.bs_alpha_tilde[i][0])
+        L += self.q * (- log(special.gamma(self.a_alpha)) + self.a_alpha * log(self.b_alpha) )
+        for i in range(0, self.q):
+            L +=  -log(self.bs_alpha_tilde[i][0]) + special.digamma(self.a_alpha_tilde)- self.b_alpha * (self.a_alpha_tilde / self.bs_alpha_tilde[i][0])
         
-        #print 3, L
         #ln(N(\mu))
-        L += - self.beta/2*(np.trace(self.sigma_mu) + dot(self.mean_mu.T, self.mean_mu)[0][0])
+        L += self.d/2 * (np.log(self.beta) - np.log(2 * np.pi) ) - self.beta/2*(np.trace(self.sigma_mu) + dot(self.mean_mu.T, self.mean_mu)[0][0])
         
-        #print 4, L
         #ln(Ga(\tau))
         L +=  -log(self.b_tau_tilde[0]) + special.digamma(self.a_tau_tilde) - (self.a_tau_tilde / self.b_tau_tilde[0])
 
-        
-        #print 5, L
         ###Terms from entropies
-        
         #H[Q(Z)]
-        L += self.N /2 * log(linalg.det(self.sigma_z))
+        L += self.N*( self.d/2*(1+ np.log(2*np.pi)) + 1/2 * log(linalg.det(self.sigma_z)))
         
-        #print 6, L, log(linalg.det(self.sigma_z))
         #H[Q(\mu)]
-        L += (0.5)*log(linalg.det(self.sigma_mu))
+        L += (0.5)*log(linalg.det(self.sigma_mu))  + self.d/2*(1+ np.log(2*np.pi))
         
-       # print 7, L, log(linalg.det(self.sigma_mu))
         #H[Q(W)]
-        L += self.d /2 *log(linalg.det(self.sigma_w))
-
-      #  print 8, L, log(linalg.det(self.sigma_w))
-    
-    
+        L += self.d*(self.d /2 *(1+ np.log(2*np.pi)) + 1/2*log(linalg.det(self.sigma_w)) )
+        
         #H[Q(\alpha)]
-        L += self.d * (self.a_alpha_tilde + log(special.gamma(self.a_alpha_tilde)) + (1-self.a_alpha_tilde)*special.digamma(self.a_alpha_tilde)) 
+        L += self.q * (self.a_alpha_tilde + log(special.gamma(self.a_alpha_tilde)) + (1-self.a_alpha_tilde)*special.digamma(self.a_alpha_tilde)) 
         for i in range(0,self.d):
             L += -log(self.bs_alpha_tilde[i][0])
-      #  print 9, L
+      
         #H[Q(\tau)]
-        #L += self.a_tau_tilde - log(self.b_tau_tilde[0]) + log(special.gamma(self.a_tau_tilde)) + (1-self.a_tau_tilde) * special.digamma(self.a_tau_tilde)
         L += self.a_tau_tilde - log(self.b_tau_tilde[0]) + (1-self.a_tau_tilde) * special.digamma(self.a_tau_tilde)
+        ## the term Gamma(a_tau_tilde) is inf so we ignore it...
         
-        
-       # print 10, L
         return L
     
-    def printstuff(self):
-        print (" ------------- INFO --------------------------")
-        print ("Shape of means_z : " , self.means_z.shape)
-        print ("Shape of sigma_z : " , self.sigma_z.shape)
-        print ("mean_mu : " , self.mean_mu)
-        print ("sigma_mu : " , self.sigma_mu)
-        print ("Shape of means_w : " , self.means_w.shape)
-        print ("sigma_w : " , self.sigma_w)
-        print ("a_alpha_tilde : " , self.a_alpha_tilde) 
-        print ("bs_alpha_tilde  : " , self.bs_alpha_tilde) 
-        print ("a_tau_tilde : " , self.a_tau_tilde)
-        print ("b_tau_tilde : " , self.b_tau_tilde) 
-        pass
-        
     def fit(self, X,iterations = 1000, threshold = 1):
         self.__update_tau(X)
         L_previous = -1000000000
@@ -191,8 +149,8 @@ class BayesianPCA(object):
             self.__update_w(X)
             self.__update_z(X)  
             if i % 100 == 1 :
-                print("Iterations: " , i)
-                print("Lower Bound Value : " , self.L(X))
+                print "Iterations: " , i
+                print "Lower Bound Value : " , self.L(X)
             i += 1
             L_previous = L_new
             L_new = self.L(X)

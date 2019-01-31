ff5dbe4c607814cb897435df8a4215ccc5c61c0d
Fabio FERREIRA <fferreir@coast.cs.ucl.ac.uk>
2019-01-31 16:17:21 +0000

    Updates Bayesian CCA

--- BayesianCCA.py
+++ BayesianCCA.py
@@ -42,10 +42,23 @@ class BayesianCCA(object):
             self.nu_new[i] = self.nu + N
 
     def update_z(self, X):
-        E_tau = self.a_tau_tilde / self.b_tau_tilde
-        E_W = self.means_w
-        E_mu = self.mean_mu
-        for n in range(0,self.N):
-            t_n = np.reshape(X.T[n],(self.d,1)) 
-            self.means_z.T[n] = reshape(E_tau * dot ( dot( self.sigma_z , E_W.T ) , (t_n - E_mu)) ,self.q)
-        self.sigma_z = np.linalg.inv(np.identity(self.q) + E_tau*(dot(E_W.T,E_W)))
+        self.E_phi = [[] for _ in range(self.d.shape[1])]
+        for i in range(0,self.d.shape[1]):
+            self.E_phi[i] = np.dot(self.nu_new[i],np.linalg.inv(self.K_new[i]))
+        
+        self.sigma_z = np.linalg.inv(np.identity(self.m) + \
+                            np.trace(np.dot(self.E_phi[0],self.sigma_w[0])) + self.means_w[0].T.dot(self.E_phi[0]).dot(self.means_w[0]) + \
+                            np.trace(np.dot(self.E_phi[1],self.sigma_w[0])) + self.means_w[1].T.dot(self.E_phi[1]).dot(self.means_w[1]))
+        self.means_z = self.sigma_z.dot(((X[0] - self.mean_mu[0]).T.dot(self.E_phi[0]).dot(self.means_w[0]) + \
+                                         (X[1] - self.mean_mu[1]).T.dot(self.E_phi[1]).dot(self.means_w[1])).T)
+
+    def update_mu(self,X):
+        for i in range(0,self.d.shape[1]):
+            self.sigma_mu[i] = np.linalg.inv(self.beta[i] * np.identity(self.d[i]) + self.N * self.E_phi[i])
+            S = 0 
+            for n in range(0,self.N):
+                x_n = np.reshape(X[i].T[n],(self.d[i],1)) 
+                z_n = np.reshape(self.means_z.T[n],(self.m,1)) 
+                S += x_n - np.dot(self.means_w[i],z_n)
+            self.mean_mu[i] = self.E_phi[i].dot(self.sigma_mu[i],S)
+                                    

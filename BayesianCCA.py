642603e2d3471b0c23168d7dad747f5b577c3e57
Fabio FERREIRA <fferreir@coast.cs.ucl.ac.uk>
2019-02-01 18:05:27 +0000

    phis update

--- BayesianCCA.py
+++ BayesianCCA.py
@@ -39,7 +39,7 @@ class BayesianCCA(object):
             self.a_new[i] = self.a + d[i]/2.0
             self.b_new[i] = np.abs(np.random.randn(m, 1))
             self.K_new[i] = np.abs(np.random.randn(d[i], d[i]))
-            self.nu_new[i] = self.nu + N
+            self.nu_new[i] = self.nu[i] + N
 
     def update_z(self, X):
         self.E_phi = [[] for _ in range(self.d.shape[1])]
@@ -61,4 +61,118 @@ class BayesianCCA(object):
                 z_n = np.reshape(self.means_z.T[n],(self.m,1)) 
                 S += x_n - np.dot(self.means_w[i],z_n)
             self.mean_mu[i] = self.E_phi[i].dot(self.sigma_mu[i],S)
-                                    
+
+    def update_w(self, X):
+        for i in range(0,self.d.shape[1]):
+            E_zz = (self.N * self.sigma_z[i] + np.dot(self.means_z[i],self.means_z[i].T))
+            self.sigma_w[i] = np.linalg.inv(np.diagonal(self.a_new[i]/self.b_new[i]) + self.E_phi[i][self.d[i],self.d[i]] * E_zz)
+            for k in range(0,self.d[i]):
+                S = 0
+                mu_k = self.mean_mu[i][k]
+                for n in range(0,self.N):
+                    x_n_k = X[i].T[n][k]
+                    z_n = np.reshape(self.means_z.T[n],(self.m,1))
+                    S += ((x_n_k - mu_k).dot(self.E_phi[i]).dot(z_n) - E_zz).T
+                self.means_w[i][k] = np.reshape(np.dot(self.sigma_w,S), self.m)
+
+    def update_alpha(self):
+        for i in range(0,self.d.shape[1]):
+            for j in range(0 , self.m):
+                self.b_new[i][j] = self.b + (np.trace(self.sigma_w[i]) + np.dot(self.means_w.T[i][:,j].T,self.means_w.T[i][:,j])) / 2
+         
+        
+    def update_phi(self, X):
+        for i in range(0,self.d.shape[1]):
+            A = 0
+            E_W = self.means_w[i]
+            for j in range(0,self.N):
+                x_n = np.reshape(X[i].T[j],(self.d[i],1))
+                z_n = np.reshape(self.means_z.T[j],(self.m,1))
+                A += np.dot(x_n.T,x_n) + np.trace(self.sigma_mu[i]) + np.dot(self.mean_mu.T[i],self.mean_mu[i])
+                A += np.trace(np.dot(np.trace(self.sigma_w[i]) + np.dot(E_W.T,E_W), self.sigma_z + np.dot(z_n,z_n.T)))
+                A += 2 * np.dot(np.dot(self.mean_mu.T,self.means_w),z_n)
+                A += -2 * np.dot(np.dot(z_n.T,self.means_w[i]),z_n) - 2 * np.dot(z_n.T,self.mean_mu)
+            self.K_new[i] = self.K[i] + 0.5*A            
+
+    def L(self, X):
+        
+        L = 0
+        ###Terms from expectations###
+        #N(X_n|Z_n)
+        L += -self.N*self.d/2 * (special.digamma(self.a_tau_tilde) - np.log(self.b_tau_tilde)) - self.d/2 *np.log(2*np.pi)
+        S = 0
+        for n in range(0,self.N):
+            S += np.dot(X[:,n].T, X[:,n])
+            S += np.dot(np.dot(X[:,n].T,self.means_w),self.means_z[:,n]) 
+            S += np.dot(X[:,n].T, self.mean_mu)[0]
+            S -= np.dot(np.dot(self.means_w.T,self.means_z[:,n].T) ,X[:,n])
+            S -= np.dot(np.dot(self.means_w.T,self.means_z[:,n].T) ,self.mean_mu)    
+            S += np.dot(self.mean_mu.T,X[:,n])[0]
+            S -= np.dot(np.dot(self.mean_mu.T,self.means_w) ,self.means_z[:,n]) 
+            S += np.trace(self.sigma_mu) + np.dot( self.mean_mu.T,self.mean_mu)[0]
+            S += np.dot(self.mean_mu.T, X[:,n].T)[0]
+            S -= np.dot (np.dot (self.mean_mu.T, self.means_w), self.means_z[:,n])[0]
+            S += np.trace(self.sigma_mu) + np.dot(self.mean_mu.T, self.mean_mu)[0][0]
+        L +=  self.N/2 *(self.a_tau_tilde/self.b_tau_tilde)*S
+        
+        #sum ln N(z_n)
+        L += - self.N / 2 * np.trace(self.sigma_z) - self.N*self.d/2 *np.log(2*np.pi)
+        for n in range(0,self.N):
+            L += - 1/2 * np.dot(self.means_z.T[n].T, self.means_z.T[n])
+        
+        #sum ln(W|a)
+        L += self.q * self.d /2 * (- np.log(2*np.pi) )
+        for i in range(0, self.q):
+            L+= -self.q /2 * ((special.digamma(self.a_alpha_tilde) - log(self.bs_alpha_tilde[i][0]) ) + self.a_alpha_tilde / self.bs_alpha_tilde[i][0]) /   (- 1/2) *  (np.trace(self.sigma_w) + np.dot(self.means_w.T[i].T, self.means_w.T[i]))
+        
+        #sum ln (Ga(a_i))
+        L += self.q * (- log(special.gamma(self.a_alpha)) + self.a_alpha * log(self.b_alpha) )
+        for i in range(0, self.q):
+            L +=  -log(self.bs_alpha_tilde[i][0]) + special.digamma(self.a_alpha_tilde)- self.b_alpha * (self.a_alpha_tilde / self.bs_alpha_tilde[i][0])
+        
+        #ln(N(\mu))
+        L += self.d/2 * (np.log(self.beta) - np.log(2 * np.pi) ) - self.beta/2*(np.trace(self.sigma_mu) + np.dot(self.mean_mu.T, self.mean_mu)[0][0])
+        
+        #ln(Ga(\tau))
+        L +=  -log(self.b_tau_tilde[0]) + special.digamma(self.a_tau_tilde) - (self.a_tau_tilde / self.b_tau_tilde[0])
+
+        ###Terms from entropies
+        #H[Q(Z)]
+        L += self.N*( self.d/2*(1+ np.log(2*np.pi)) + 1/2 * log(linalg.det(self.sigma_z)))
+        
+        #H[Q(\mu)]
+        L += (0.5)*log(linalg.det(self.sigma_mu))  + self.d/2*(1+ np.log(2*np.pi))
+        
+        #H[Q(W)]
+        L += self.d*(self.d /2 *(1+ np.log(2*np.pi)) + 1/2*log(linalg.det(self.sigma_w)) )
+        
+        #H[Q(\alpha)]
+        L += self.q * (self.a_alpha_tilde + log(special.gamma(self.a_alpha_tilde)) + (1-self.a_alpha_tilde)*special.digamma(self.a_alpha_tilde)) 
+        for i in range(0,self.d):
+            L += -log(self.bs_alpha_tilde[i][0])
+      
+        #H[Q(\tau)]
+        L += self.a_tau_tilde - log(self.b_tau_tilde[0]) + (1-self.a_tau_tilde) * special.digamma(self.a_tau_tilde)
+        ## the term Gamma(a_tau_tilde) is inf so we ignore it...
+        
+        return L
+    
+    def fit(self, X,iterations = 1000, threshold = 1):
+        self.update_tau(X)
+        L_previous = -1000000000
+        L_new = 0
+        i = 0
+        for i in range(iterations):
+            self.update_tau(X)
+            self.update_mu(X)
+            self.update_alpha()            
+            self.update_w(X)
+            self.update_z(X)  
+            if i % 100 == 1 :
+                print("Iterations: %d" , i)
+                print("Lower Bound Value : %d" , self.L(X))
+            i += 1
+            L_previous = L_new
+            L_new = self.L(X)
+            if abs(L_new - L_previous) < threshold:
+                break

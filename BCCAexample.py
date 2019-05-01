3337fddf2f8d0ab62b3813f836b02312edf1986a
Fabio FERREIRA <fferreir@coast.cs.ucl.ac.uk>
2019-05-01 18:23:15 +0100

    Added more plots and fixed small issues

--- BCCAexample.py
+++ BCCAexample.py
@@ -40,8 +40,8 @@ K = 4                 # components
 Z = np.zeros((N, K))
 j = 0
 for i in range(0, N):
-    Z[i,0] = math.sin(i+1/(N/20))
-    Z[i,1] = math.cos(i+1/(N/20))
+    Z[i,0] = np.sin((i+1)/(N/20))
+    Z[i,1] = np.cos((i+1)/(N/20))
     if i < Ntrain:
         Z[i,3] = 2*((i+1)/Ntrain-0.5)
     else:
@@ -58,7 +58,7 @@ tau = np.array([3, 6])
 #ARD parameters
 alpha = np.zeros((S, K))
 alpha[0,:] = np.array([1,1,1e8,1])
-alpha[1,:] = np.array([1,1,1e8,1])
+alpha[1,:] = np.array([1,1,1,1e8])
 
 X = [[] for _ in range(d.size)]
 X_train = [[] for _ in range(d.size)]
@@ -85,12 +85,38 @@ L = BCCA.fit(X)
 #Hinton diagrams for W1 and W2
 W1 = BCCA.means_w[0]
 W2 = BCCA.means_w[1]
-hinton(W1.T)
-hinton(W2.T)
+W = np.concatenate((W1,W2),axis=0)
+hinton(W)
+
+#Hinton diagrams for alpha1 and alpha2
+a1 = np.reshape(BCCA.E_alpha[0],(m,1))
+a2 = np.reshape(BCCA.E_alpha[1],(m,1))
+a = np.concatenate((a1,a2),axis=1)
+hinton(-a.T)
 
 print("Estimated variances:", BCCA.E_tau)
-print("Estimated alphas:", BCCA.E_alpha)
+#print("Estimated alphas:", BCCA.E_alpha)
 
 #plot lower bound
 plt.plot(L)
+plt.show()
+
+#plot true latent variables
+x = np.linspace(0,99,100)
+f, ((ax1, ax2, ax3, ax4)) = plt.subplots(4, 1, sharex='col', sharey='row')
+f.suptitle('True latent components')
+ax1.scatter(x,Z_train[:,0])
+ax2.scatter(x,Z_train[:,1])
+ax3.scatter(x,Z_train[:,2])
+ax4.scatter(x,Z_train[:,3])
+plt.show()
+
+#plot estimated latent variables
+x = np.linspace(0,99,100)
+f, ((ax1, ax2, ax3, ax4)) = plt.subplots(4, 1, sharex='col', sharey='row')
+f.suptitle('Estimated latent components')
+ax1.scatter(x,BCCA.means_z[:,1])
+ax2.scatter(x,BCCA.means_z[:,4])
+ax3.scatter(x,BCCA.means_z[:,5])
+ax4.scatter(x,BCCA.means_z[:,6])
 plt.show()

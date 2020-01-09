cbbbfed9bcb5b344e771da9a15faf37814ad07a8
Fabio FERREIRA <fferreir@coast.cs.ucl.ac.uk>
2020-01-09 09:52:13 +0000

    Addin extra parameters main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pickle
 import argparse
 import time
 import os
-import hdf5storage
+#import hdf5storage
 from scipy import io
 from sklearn.preprocessing import StandardScaler
 
@@ -14,33 +14,36 @@ from sklearn.preprocessing import StandardScaler
 def get_args():
     parser = argparse.ArgumentParser()
     proj_dir = '/cs/research/medic/human-connectome/experiments/fabio_hcp500'
+    #proj_dir = '/SAN/medic/human-connectome/experiments/fabio_hcp500'
     parser.add_argument('--dir', type=str, default=proj_dir, 
                         help='Main directory')
     parser.add_argument('--data', type=str, default='data', 
                         help='Dataset')
     parser.add_argument('--type', type=str, default='preproc', 
                         help='Data that will be used')
-    parser.add_argument('--scenario', type=str, default=f'missing20_rows_view2', 
-                        help='Including or not missing data')
     parser.add_argument('--noise', type=str, default='FA', 
                         help='Noise assumption')
     parser.add_argument('--method', type=str, default='GFA', 
-                        help='Model to be used')
-    parser.add_argument('--standardise', type=bool, default=True, 
-                        help='Standardise the data') 
-    parser.add_argument('--training', type=bool, default=False, 
-                        help='Create Train and test sets')                                       
+                        help='Model to be used')                                       
     parser.add_argument('--m', type=int, default=25,
                         help='number of components to be used')
     parser.add_argument('--n_init', type=int, default=1,
                         help='number of random initializations')
     
+    #Preprocessing and training
+    parser.add_argument('--standardise', type=bool, default=True, 
+                        help='Standardise the data') 
+    parser.add_argument('--prediction', type=bool, default=False, 
+                        help='Create Train and test sets')
+    parser.add_argument('--perc_train', type=int, default=80,
+                        help='Percentage of training data')                    
+
     #Mising data
     parser.add_argument('--remove', type=bool, default=True,
                         help='Remove data')
     parser.add_argument('--perc_miss', type=int, default=20,
                         help='Percentage of missing data')
-    parser.add_argument('--type_miss', type=str, default='rows',
+    parser.add_argument('--type_miss', type=str, default='random',
                         help='Type of missing data')
     parser.add_argument('--view_miss', type=str, default='2',
                         help='View with missing data')                                            
@@ -50,8 +53,16 @@ def get_args():
 FLAGS = get_args()
 if FLAGS.remove:
     scenario = f'missing{FLAGS.perc_miss}_{FLAGS.type_miss}_view{FLAGS.view_miss}'
+else:
+    scenario = f'complete'
+
+if FLAGS.prediction:
+    split_data = f'training{FLAGS.perc_train}'
+else:
+    split_data = 'all'              
+
 #Creating path
-res_dir = f'{FLAGS.dir}/{FLAGS.data}/{FLAGS.type}/{FLAGS.method}_{FLAGS.noise}/{FLAGS.m}models/{scenario}/'
+res_dir = f'{FLAGS.dir}/{FLAGS.data}/{FLAGS.type}/{FLAGS.method}_{FLAGS.noise}/{FLAGS.m}models/{scenario}/{split_data}/'
 if not os.path.exists(res_dir):
         os.makedirs(res_dir)
         
@@ -91,9 +102,8 @@ if standardise is True:
     X[1] = StandardScaler().fit_transform(X[1])
 
 #Train/test
-if FLAGS.training is True:
-    p_train = 80
-    n_rows = int(p_train * X[0].shape[0]/100)
+if FLAGS.prediction is True:
+    n_rows = int(FLAGS.perc_train * X[0].shape[0]/100)
     samples = np.arange(X[0].shape[0])
     np.random.shuffle(samples)
     train_ind = samples[0:n_rows]
@@ -114,12 +124,15 @@ for init in range(0, FLAGS.n_init):
     d = np.array([X_train[0].shape[1], X_train[1].shape[1]])
     if FLAGS.remove is True:
         if 'random' in FLAGS.type_miss:
-            missing =  np.random.choice([0, 1], size=(X_train[1].shape[0],d[1]), 
-                                        p=[1-FLAGS.perc_miss/100, FLAGS.perc_miss/100])
             if '1' in FLAGS.view_miss:
+                missing =  np.random.choice([0, 1], size=(X_train[0].shape[0],d[0]), 
+                                        p=[1-FLAGS.perc_miss/100, FLAGS.perc_miss/100])
                 X_train[0][missing == 1] = 'NaN'
             elif '2' in FLAGS.view_miss:
-                X_train[1][samples[0:n_rows],:] = 'NaN' 
+                missing =  np.random.choice([0, 1], size=(X_train[1].shape[0],d[1]), 
+                                        p=[1-FLAGS.perc_miss/100, FLAGS.perc_miss/100])
+                X_train[1][missing == 1] = 'NaN' 
+        
         elif 'rows' in FLAGS.type_miss:
             n_rows = int(FLAGS.perc_miss/100 * X[0].shape[0])
             samples = np.arange(X[0].shape[0])
@@ -128,13 +141,16 @@ for init in range(0, FLAGS.n_init):
                 X_train[0][samples[0:n_rows],:] = 'NaN'
             elif '2' in FLAGS.view_miss:
                 X_train[1][samples[0:n_rows],:] = 'NaN'       
+        
         GFAmodel[init] = GFAmissing(X_train, FLAGS.m, d)
     elif 'FA' is FLAGS.noise:   
         GFAmodel[init] = GFAmissing(X_train, FLAGS.m, d)
     else:
         GFAmodel[init] = GFAcomplete(X_train, FLAGS.m, d)
-    if FLAGS.training is True:
+    
+    if FLAGS.prediction is True:
         GFAmodel[init].X_test = X_test        
+    
     L = GFAmodel[init].fit(X_train)
     GFAmodel[init].L = L
     GFAmodel[init].time_elapsed = (time.process_time() - time_start) 
@@ -143,5 +159,3 @@ filepath = f'{res_dir}{FLAGS.method}_results.dictionary'
 with open(filepath, 'wb') as parameters:
 
     pickle.dump(GFAmodel, parameters)
-
-FLAGS.view_miss

fix normalisation

analysis_HCP.py
@@ -14,7 +14,22 @@ from scipy import io
 from sklearn.preprocessing import StandardScaler
 from models import GFA_DiagonalNoiseModel, GFA_OriginalModel
 from utils import GFAtools
-														                                             
+
+def compute_mses(X_train, X_test, model):
+
+    # Calculate means of the SMs (non-imaging subject measures) (data source 2)
+    Beh_trainmean = np.nanmean(X_train[1], axis=0)               
+    # MSE for each SM
+    obs_ds = np.array([1, 0]) #data source 1 was observed 
+    gpred = np.where(obs_ds == 0)[0][0] #get the non-observed data source  
+    X_pred = GFAtools(X_test, model).PredictDSources(obs_ds, args.noise)
+    MSE_SMs = np.zeros((1, model.d[1]))
+    MSE_SMs_trmeans = np.zeros((1, model.d[1]))
+    for j in range(model.d[1]):
+        MSE_SMs[0,j] = np.mean((X_test[gpred][:,j] - X_pred[0][:,j]) ** 2) / np.mean(X_test[gpred][:,j] ** 2)
+        MSE_SMs_trmeans[0,j] = np.mean((X_test[gpred][:,j] - Beh_trainmean[j]) ** 2) / np.mean(X_test[gpred][:,j] ** 2)
+
+    return MSE_SMs, MSE_SMs_trmeans   
 
 def main(args): 
 
@@ -46,13 +61,10 @@ def main(args):
     #load file containing labels for Y (item-level questionnaire data)
     df_ylb = pd.read_excel(f'{data_dir}/LabelsY.xlsx')               
     ylabels = df_ylb['Label'].values
-    #standardise data if needed
+    #load matrices
     X = [[] for _ in range(S)]
     X[0] = brain_data['X']
     X[1] = clinical_data['Y']
-    if args.standardise and args.scenario == 'complete':
-        X[0] = StandardScaler().fit_transform(X[0])
-        X[1] = StandardScaler().fit_transform(X[1])               
 
     for run in range(0, args.num_runs):
         print("Run: ", run+1)
@@ -78,6 +90,13 @@ def main(args):
                 X_train[i] = X[i][train_ind,:] 
                 X_test[i] = X[i][test_ind,:]
             
+            #standardise data if needed
+            if args.standardise and args.scenario == 'complete':
+                for i in range(S):
+                    scale = StandardScaler().fit(X_train[i])
+                    X_train[i] = scale.transform(X_train[i])
+                    X_test[i] = scale.transform(X_test[i])
+
             #ensure the training set size is right
             assert round((train_ind.size/N) * 100) == args.ptrain   
 
@@ -90,7 +109,7 @@ def main(args):
                         missing =  np.random.choice([0, 1], size=(X_train[args.gmiss-1].shape[0], 
                                     X_train[args.gmiss-1].shape[1]), p=[1-args.pmiss/100, args.pmiss/100])
                         mask_miss =  np.ma.array(X_train[args.gmiss-1], mask = missing).mask
-                        missing_true = np.where(missing==1, X_train[args.gmiss-1],0)
+                        miss_true = np.where(missing==1, X_train[args.gmiss-1],0)
                         X_train[args.gmiss-1][mask_miss] = 'NaN'                   
                         #ensure the percentage of missing values is correct
                         assert round((mask_miss[mask_miss==1].size/X_train[args.gmiss-1].size) * 100) == args.pmiss
@@ -100,16 +119,17 @@ def main(args):
                         n_rows = int(args.pmiss/100 * X_train[args.gmiss-1].shape[0])
                         samples = np.arange(X_train[args.gmiss-1].shape[0])
                         np.random.shuffle(samples)
-                        missing_true = X_train[args.gmiss-1][samples[0:n_rows],:]
+                        miss_true = X_train[args.gmiss-1][samples[0:n_rows],:]
                         X_train[args.gmiss-1][samples[0:n_rows],:] = 'NaN'
                     
                     # Standardise the data after removing the values
-                    X_train[0] = StandardScaler().fit_transform(X_train[0])
-                    X_train[1] = StandardScaler().fit_transform(X_train[1])
+                    for i in range(S):
+                        scale = StandardScaler().fit(X_train[i])
+                        X_train[i] = scale.transform(X_train[i])
+                        X_test[i] = scale.transform(X_test[i])
+                    
                     # Initialise the model    
-                    GFAmodel = GFA_DiagonalNoiseModel(X_train, params)
-                    #save true missing values
-                    GFAmodel.miss_true = missing_true    
+                    GFAmodel = GFA_DiagonalNoiseModel(X_train, params)  
                 else:
                     GFAmodel = GFA_DiagonalNoiseModel(X_train, params)    
             else:
@@ -120,16 +140,28 @@ def main(args):
             GFAmodel.fit(X_train)
             GFAmodel.time_elapsed = time.process_time() - time_start
             print(f'Computational time: {float("{:.2f}".format(GFAmodel.time_elapsed/60))} min')
-            GFAmodel.indTest = test_ind
-            GFAmodel.indTrain = train_ind
 
+            # Compute MSE for each SM
+            GFAmodel.MSEs_SMs_te, GFAmodel.MSEs_SMs_tr = compute_mses(X_train, X_test, GFAmodel)
+
+            # Predict missing values
+            if args.scenario == 'incomplete':
+                infmiss = {'perc': [args.pmiss], #percentage of missing data 
+                    'type': [args.tmiss], #type of missing data 
+                    'ds': [args.gmiss]} #data sources with missing values          
+                miss_pred = GFAtools(X_train, GFAmodel).PredictMissing(infmiss)
+                GFAmodel.corrmiss = np.corrcoef(miss_true[miss_true != 0], 
+                                miss_pred[0][np.logical_not(np.isnan(miss_pred[0]))])[0,1]
+                # Remove mask with NaNs from the model output dictionary
+                del GFAmodel.X_nan
+            
             # Save file containing the model outputs
             with open(filepath, 'wb') as parameters:
                 pickle.dump(GFAmodel, parameters)        
 
     #visualization
     print('Plotting results--------')
-    visualization_HCP.get_results(args, X, ylabels, res_dir)
+    visualization_HCP.get_results(args, ylabels, res_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run GFA using HCP data")
@@ -139,9 +171,9 @@ if __name__ == "__main__":
                         help='Noise assumption for GFA models (diagonal or spherical)') 
     parser.add_argument('--num_sources', type=int, default=2, 
                         help='Number of data sources')                                                          
-    parser.add_argument('--K', type=int, default=80,
+    parser.add_argument('--K', type=int, default=5,
                         help='number of components to initialise the model')
-    parser.add_argument('--num_runs', type=int, default=6,
+    parser.add_argument('--num_runs', type=int, default=1,
                         help='number of random initializations (runs)')
     # Preprocessing and training
     parser.add_argument('--standardise', type=bool, default=True, 
@@ -155,7 +187,7 @@ if __name__ == "__main__":
     # the missing data)
     parser.add_argument('--pmiss', type=int, default=20,
                         help='Percentage of missing data')
-    parser.add_argument('--tmiss', type=str, default='rows',
+    parser.add_argument('--tmiss', type=str, default='random',
                         help='Type of missing data (completely random values or rows)')
     parser.add_argument('--gmiss', type=int, default=1,
                         help='Data source (group) cointining missing data')

visualization_HCP.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 import pickle
 import pandas as pd
 import xlsxwriter
+import os
+import sys
 from scipy import io
-from utils import GFAtools
 
 def find_relfactors(model, res_dir, BestModel=False):
     
@@ -80,7 +81,7 @@ def find_relfactors(model, res_dir, BestModel=False):
 
     return relfactors_shared, relfactors_specific                       
 
-def get_results(args, X, ylabels, res_path):
+def get_results(args, ylabels, res_path):
 
     """ 
     Plot and save the results of the experiments on HCP data.
@@ -89,10 +90,6 @@ def get_results(args, X, ylabels, res_path):
     ----------
     args : local namespace 
         Arguments selected to run the model.
-
-    X : list 
-        List of arrays containing the data matrix of each 
-        data source.    
     
     ylabels : array-like
         Array of strings with the labels of the non-imaging
@@ -105,8 +102,8 @@ def get_results(args, X, ylabels, res_path):
     """
     nruns = args.num_runs #number of runs
     #initialise variables to save MSEs, correlations and ELBO values
-    MSE_beh = np.zeros((nruns, X[1].shape[1]))
-    MSE_beh_trmean = np.zeros((nruns, X[1].shape[1]))
+    MSE_SMs_te = np.zeros((nruns, ylabels.size))
+    MSE_SMs_tr = np.zeros((nruns, ylabels.size))
     if args.scenario == 'incomplete': 
         Corr_miss = np.zeros((1,nruns))    
     ELBO = np.zeros((1,nruns))
@@ -116,6 +113,8 @@ def get_results(args, X, ylabels, res_path):
         print('\nInitialisation: ', i+1, file=ofile)
         print('------------------------------------------------', file=ofile)
         filepath = f'{res_path}[{i+1}]Results.dictionary'
+        #ensure file is not empty
+        assert os.stat(filepath).st_size > 5
         #Load file containing the model outputs
         with open(filepath, 'rb') as parameters:
             GFA_otp = pickle.load(parameters)  
@@ -125,36 +124,13 @@ def get_results(args, X, ylabels, res_path):
         ELBO[0,i] = GFA_otp.L[-1]
         print('ELBO (last value):', np.around(ELBO[0,i],2), file=ofile)
 
-        #-Predictions (predict data source 2 from data source 1)
-        #---------------------------------------------------------------------
-        # Get training and test sets  
-        X_train = [[] for _ in range(GFA_otp.s)]
-        X_test = [[] for _ in range(GFA_otp.s)]  
-        for s in range(GFA_otp.s):
-            X_train[s] = X[s][GFA_otp.indTrain,:]
-            if hasattr(GFA_otp, 'X_nan'):
-                if np.any(GFA_otp.X_nan[s] == 1):
-                    X_train[s][GFA_otp.X_nan[s] == 1] = 'NaN'
-            X_test[s] = X[s][GFA_otp.indTest,:]
-        # Calculate means of the SMs (non-imaging subject measures) (data source 2)
-        Beh_trainmean = np.nanmean(X_train[1], axis=0)               
-        # MSE for each SM
-        obs_ds = np.array([1, 0]) #data source 1 was observed 
-        gpred = np.where(obs_ds == 0)[0][0] #get the non-observed data source  
-        X_pred = GFAtools(X_test, GFA_otp).PredictDSources(obs_ds, args.noise)
-        for j in range(GFA_otp.d[1]):
-            MSE_beh[i,j] = np.mean((X_test[gpred][:,j] - X_pred[0][:,j]) ** 2)/np.mean(X_test[gpred][:,j] ** 2)
-            MSE_beh_trmean[i,j] = np.mean((X_test[gpred][:,j] - Beh_trainmean[j]) ** 2)/np.mean(X_test[gpred][:,j] ** 2)
+        # Get predictions (predict SMs from brain connectivity)
+        MSE_SMs_te[i,:] = GFA_otp.MSEs_SMs_te
+        MSE_SMs_tr[i,:] = GFA_otp.MSEs_SMs_tr
         
-        # Predict missing values
+        # Get predictions (missing values)
         if args.scenario == 'incomplete':
-            infmiss = {'perc': [args.pmiss], #percentage of missing data 
-                'type': [args.tmiss], #type of missing data 
-                'ds': [args.gmiss]} #data sources with missing values          
-            miss_pred = GFAtools(X_train, GFA_otp).PredictMissing(infmiss)
-            miss_true = GFA_otp.miss_true
-            Corr_miss[0,i] = np.corrcoef(miss_true[miss_true != 0], 
-                            miss_pred[0][np.logical_not(np.isnan(miss_pred[0]))])[0,1]
+            Corr_miss[0,i] = GFA_otp.corrmiss
 
         #Calculate total variance explained
         if hasattr(GFA_otp, 'VarExp_total') is False:           
@@ -163,7 +139,7 @@ def get_results(args, X, ylabels, res_path):
             for s in range(GFA_otp.s):
                 w = GFA_otp.means_w[s]
                 if 'spherical' in args.noise:
-                    T = 1/GFA_otp.E_tau[s] * np.identity(w.shape[0])
+                    T = 1/GFA_otp.E_tau[0,s] * np.identity(w.shape[0])
                 else:
                     T = np.diag(1/GFA_otp.E_tau[s][0,:])
                 total_var += np.trace(np.dot(w,w.T) + T)
@@ -179,7 +155,7 @@ def get_results(args, X, ylabels, res_path):
                 np.around((GFA_otp.VarExp_factors/GFA_otp.VarExp_total) * 100,2), file=ofile)
         print('Relevant shared factors: ', np.array(relfact_sh)+1, file=ofile)
         for m in range(args.num_sources):
-            print(f'Relevant specific factors (data source {m+1}): ', np.array(relfact_sp[m])+1, file=ofile)                 
+            print(f'Relevant specific factors (data source {m+1}): ', np.array(relfact_sp[m])+1, file=ofile)                       
     
     best_ELBO = int(np.argmax(ELBO)+1)
     print('\nOverall results for the best model---------------', file=ofile)  
@@ -219,7 +195,7 @@ def get_results(args, X, ylabels, res_path):
     io.savemat(f'{res_path}/Z.mat', Z)                 
 
     print(f'\nMulti-output predictions:--------------------------\n', file=ofile)
-    sort_beh = np.argsort(np.mean(MSE_beh, axis=0))
+    sort_beh = np.argsort(np.mean(MSE_SMs_te, axis=0))
     top = 10
     print(f'Top {top} predicted variables: ', file=ofile)
     for l in range(top):
@@ -232,11 +208,11 @@ def get_results(args, X, ylabels, res_path):
     # Plot MSE of each non-imaging subject measure
     plt.figure(figsize=(10,6))
     pred_path = f'{res_path}/Predictions.png'
-    x = np.arange(MSE_beh.shape[1])
-    plt.errorbar(x, np.mean(MSE_beh,axis=0), yerr=np.std(MSE_beh,axis=0), fmt='bo', label='Predictions')
-    plt.errorbar(x, np.mean(MSE_beh_trmean,axis=0), yerr=np.std(MSE_beh_trmean,axis=0), fmt='yo', label='Train mean')
+    x = np.arange(MSE_SMs_te.shape[1])
+    plt.errorbar(x, np.mean(MSE_SMs_te,axis=0), yerr=np.std(MSE_SMs_te,axis=0), fmt='bo', label='Predictions')
+    plt.errorbar(x, np.mean(MSE_SMs_tr,axis=0), yerr=np.std(MSE_SMs_tr,axis=0), fmt='yo', label='Train mean')
     plt.legend(loc='upper left',fontsize=17)
-    plt.ylim((0.5,1.8))
+    plt.ylim((np.min(MSE_SMs_te)-0.2, np.max(MSE_SMs_te)+0.1))
     plt.title('Prediction of SMs from brain connectivity',fontsize=22)
     plt.xlabel('Non-imaging subject measures',fontsize=19); plt.ylabel('relative MSE',fontsize=19)
     plt.xticks(fontsize=14); plt.yticks(fontsize=14) 